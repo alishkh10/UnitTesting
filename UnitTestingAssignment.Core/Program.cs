e60d7f7c2dd584b6b6208a50442fa128f246fd0b
alisher_khamidov <alisher_khamidov@EPUZTASW0126>
2024-11-30 12:27:35 +0500

    Added a project reference

--- UnitTestingAssignment.Core/Program.cs
+++ UnitTestingAssignment.Core/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace UnitTestingAssignment
+{
+    public class FizzBuzz
+    {
+        public string GetFizzBuzz(int number)
+        {
+            if (number < 1 || number > 100)
+                throw new ArgumentOutOfRangeException(nameof(number), "Number must be within the range 1 to 100.");
+
+            if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+            return number.ToString();
+        }
+        public static void Main()
+        { }
+    }
+}
