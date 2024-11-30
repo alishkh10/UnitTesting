aff4f7ffab8c38ec6c96fbe957253382a682a5e2
alisher_khamidov <alisher_khamidov@EPUZTASW0126>
2024-11-30 13:10:13 +0500

    Added Calc Stat Tests

--- UnitTestingAssignment/UnitTest1.cs
+++ UnitTestingAssignment/UnitTest1.cs
@@ -1,15 +1,19 @@
+using System.Net.Http.Headers;
+
 namespace UnitTestingAssignment
 {
     public class Tests
     {
         private FizzBuzz fizzBuzz;
         private LeapYear leapYear;
-
+        private CalcStats calcStats;
+        private int[] inputData = { 1, 4, -3, -6, 17, 5235, -77, 5, 41, 8};
         [SetUp]
         public void Setup()
         {
             fizzBuzz = new FizzBuzz();
             leapYear = new LeapYear();
+            calcStats = new CalcStats()
         }
 
         [Test]
@@ -36,6 +40,34 @@ namespace UnitTestingAssignment
             Assert.AreEqual(true, leapYear.GetLeapYearResult(1024));
             Assert.AreEqual(false, leapYear.GetLeapYearResult(2025));
             Assert.AreEqual(false, leapYear.GetLeapYearResult(1025));
+        }
+
+        [Test]
+        public void CalcStatShouldReturnMinValue()
+        {
+            int result = calcStats.GetMinResult(inputData);
+            Assert.AreEqual(-77, result);
+        }
+
+        [Test]
+        public void CalcStatShouldReturnMaxValue()
+        {
+            int result = calcStats.GetMaxResult(inputData);
+            Assert.AreEqual(5235, result);
+        }
+
+        [Test]
+        public void CalcStatShouldReturnAvgValue()
+        {
+            int result = calcStats.GetAvgResult(inputData);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void CalcStatShouldReturnNumberOfValues()
+        {
+            int result = calcStats.GetMinResult(inputData);
+            Assert.AreEqual(10, result);
         }
     }
 }
